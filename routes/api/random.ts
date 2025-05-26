2218330db7e8be523853f670231a3f164f3e5914
briancbarrow <briancbarrow@gmail.com>
2025-05-26 16:39:58 -0600

    fix route allowed headers

--- routes/api/random.ts
+++ routes/api/random.ts
@@ -1,38 +1,42 @@
-import { FreshContext } from "$fresh/server.ts";
+import { FreshContext, Handlers } from "$fresh/server.ts";
 
-export const handler = async (
-  req: Request,
-  ctx: FreshContext,
-): Promise<Response> => {
-  const origin = req.headers.get("Origin") || "*";
-  const resp = await ctx.next();
-  const headers = resp.headers;
+export const handler: Handlers = {
+  async GET(
+    req: Request,
+    ctx: FreshContext,
+  ) {
+    const origin = req.headers.get("Origin") || "*";
 
-  headers.set("Access-Control-Allow-Origin", origin);
-  headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
 
-  const randomNumber = Math.floor(Math.random() * 6);
-  // wait for randomNumber seconds
-  const wait = new Promise((resolve) =>
-    setTimeout(resolve, randomNumber * 1000)
-  );
-  // wait for the promise to resolve
-  try {
-    await wait;
+    const headers = new Headers();
 
-    const shouldFail = Math.random() < 0.3; // 30% chance to fail
-    if (shouldFail) {
-      throw new Error("Random failure occurred");
-    }
+    headers.set("Access-Control-Allow-Origin", origin);
+    console.log("HEADERS: ", headers);
 
-    const body = `Waited for ${randomNumber} seconds before responding.`;
-    return new Response(body);
-  } catch (_error) {
-    return new Response(
-      `An error occurred after waiting ${randomNumber} seconds.`,
-      {
-        status: 500,
-      },
+    const randomNumber = Math.floor(Math.random() * 6);
+    // wait for randomNumber seconds
+    const wait = new Promise((resolve) =>
+      setTimeout(resolve, randomNumber * 1000)
     );
+    // wait for the promise to resolve
+    try {
+      await wait;
+
+      const shouldFail = Math.random() < 0.3; // 30% chance to fail
+      if (shouldFail) {
+        throw new Error("Random failure occurred");
+      }
+
+      const body = `Waited for ${randomNumber} seconds before responding.`;
+      return new Response(body, { headers });
+    } catch (_error) {
+      return new Response(
+        `An error occurred after waiting ${randomNumber} seconds.`,
+        {
+          status: 500,
+          headers,
+        },
+      );
+    }
   }
-};
+}
